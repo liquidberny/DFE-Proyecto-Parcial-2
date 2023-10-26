62b5aaa18a49358faae4a18a737915839e0453f8
liquidberny <bernardo.urquijo.bu@gmail.com>
2023-10-26 13:14:32 -0700

    update hecho

--- src/script.js
+++ src/script.js
@@ -24,6 +24,19 @@ function mapAPIToTasks(data) {
       new Date(item.dueDate),
     );
   });
+  
+}
+
+function APIToTask(data) {
+    return new Task(
+      data.id,
+      data.title,
+      data.description,
+      data.completed,
+      data.priority,
+      data.tag,
+      new Date(data.dueDate),
+    );
 }
 
 //#endregion
@@ -75,6 +88,9 @@ function displayTasksTable(tasks) {
       <td>
         <button class="btn-delete" data-task-id="${task.id}">Delete</button>
       </td>
+      <td>
+        <button class="btn-update" data-task-id="${task.id}">Update</button>
+      </td>
     `;
 
     tablaBody.appendChild(row);
@@ -82,6 +98,31 @@ function displayTasksTable(tasks) {
   });
 
   initDeleteTaskButtonHandler();
+  initUpdateTaskButtonHandler();
+}
+
+function displayTaskModal(task){
+  const id = document.getElementById("update-task-id");
+  const taskTitleField = document.getElementById("update-task-title-field");
+  const descriptionField = document.getElementById("update-description-field");
+  const completedField = document.getElementById("update-completed-field");
+  const priorityField = document.getElementById("update-priority-field");
+  const tagField = document.getElementById("update-tag-field");
+  const dueDateField = document.getElementById("update-due-date-field");
+
+  // Llenar los campos con los valores del objeto task
+  id.value=task.id;
+  taskTitleField.value = task.title;
+  descriptionField.value = task.description;
+  completedField.value = task.completed.toString();
+  priorityField.value = task.priority.toLowerCase();
+  tagField.value = task.tag;
+  
+  // Formatear la fecha en el campo "due date"
+  const dueDate = new Date(task.dueDate);
+  const formattedDueDate = dueDate.toISOString().substring(0, 10);
+  dueDateField.value = formattedDueDate;
+
 }
 
 // Funcion que limpia la tabla
@@ -121,13 +162,13 @@ function showNotFoundMessage() {
 // Funcion que oculta mensaje
 function hideMessage() {
   const message = document.getElementById('message');
-  
+
   message.style.display = 'none';
 }
 
 //#endregion
 
-//#region 3 boyones para agregar y eliminar
+//#region 3 botones para agregar, eliminar y actualizar
 
 function initAddTaskButtonsHandler() {
 
@@ -137,19 +178,34 @@ function initAddTaskButtonsHandler() {
 
   document.getElementById('modal-background').addEventListener('click', () => {
     closeAddTaskModal();
+    closeUpdateTaskModal();
   });
 
   document.getElementById('task-form').addEventListener('submit', event => {
     event.preventDefault();
     processSubmitTask();
   });
+  document.getElementById('update-task-form').addEventListener('submit', event => {
+    event.preventDefault();
+    processUpdateTask();
+  });
 
 }
 
 function openAddTaskModal() {
+
   document.getElementById('task-form').reset();
   document.getElementById('modal-background').style.display = 'block';
   document.getElementById('modal').style.display = 'block';
+
+}
+
+function openUpdateTaskModal(taskId) {
+  getTaskData(taskId);
+
+  document.getElementById('task-form').reset();
+  document.getElementById('modal-background').style.display = 'block';
+  document.getElementById('modal-update').style.display = 'block';
 }
 
 
@@ -159,6 +215,11 @@ function closeAddTaskModal() {
   document.getElementById('modal').style.display = 'none';
 }
 
+function closeUpdateTaskModal() {
+  document.getElementById('update-task-form').reset();
+  document.getElementById('modal-background').style.display = 'none';
+  document.getElementById('modal-update').style.display = 'none';
+}
 function processSubmitTask() {
   const title = document.getElementById('task-title-field').value;
   const description = document.getElementById('description-field').value;
@@ -177,8 +238,31 @@ function processSubmitTask() {
     dueDate
   );
 
-  createTask(taskToSave);
-}
+    createTask(taskToSave);
+
+  } 
+  function processUpdateTask() {
+    const id = document.getElementById('update-task-id').value
+    const title = document.getElementById('update-task-title-field').value;
+    const description = document.getElementById('update-description-field').value;
+    const completed = document.getElementById('update-completed-field').value;
+    const priority = document.getElementById('update-priority-field').value;
+    const tag = document.getElementById('update-tag-field').value;
+    const dueDate = document.getElementById('update-due-date-field').value;
+  
+    const taskToSave = new Task(
+      id,
+      title,
+      description,
+      completed,
+      priority,
+      tag,
+      dueDate
+    );
+  
+      updateTask(taskToSave);
+  
+    } 
 
 function initDeleteTaskButtonHandler() {
 
@@ -195,10 +279,25 @@ function initDeleteTaskButtonHandler() {
 
 }
 
+function initUpdateTaskButtonHandler() {
+
+  document.querySelectorAll('.btn-update').forEach(button => {
+
+    button.addEventListener('click', () => {
+
+      const taskId = button.getAttribute('data-task-id'); // Obtenemos el ID de la venta
+      openUpdateTaskModal(taskId);
+
+    });
+
+  });
+
+}
+
+
 //region 4 consumo de la API
 
-function getTaskData() {
-
+function getTasksData() {
   fetchAPI(`${apiURL}/users/219204833/tasks`, 'GET')
     .then(data => {
       const tasksList = mapAPIToTasks(data);
@@ -207,6 +306,14 @@ function getTaskData() {
 
 }
 
+function getTaskData(taskId) {
+  fetchAPI(`${apiURL}/users/219204833/tasks/${taskId}`, 'GET')
+    .then(data => {
+      const task = APIToTask(data);
+      displayTaskModal(task);
+    });
+
+}
 function deleteTask(taskId) {
 
   const confirm = window.confirm(`¿Are you sure you want to delete task: ${taskId}?`);
@@ -215,7 +322,7 @@ function deleteTask(taskId) {
 
     fetchAPI(`${apiURL}/users/219204833/tasks/${taskId}`, 'DELETE')
       .then(() => {
-        getTaskData();
+        getTasksData();
         window.alert("Task eliminated.");
       });
 
@@ -227,18 +334,28 @@ function createTask(task) {
   fetchAPI(`${apiURL}/users/219204833/tasks`, 'POST', task)
     .then(task => {
       closeAddTaskModal();
-      getTaskData();
+      getTasksData();
       window.alert(`Venta ${task.id} creada correctamente.`);
     });
 
 }
-//#endregion
+
+function updateTask(task) {
+
+  fetchAPI(`${apiURL}/users/219204833/tasks/${task.id}`, 'PUT', task)
+    .then(task => {
+      closeUpdateTaskModal();
+      getTasksData();
+      window.alert(`Venta ${task.id} actualizada correctamente.`);
+    });
+
+}
 //#endregion
 
 //#region 5 funcionalidad
 
 initAddTaskButtonsHandler();
 
-getTaskData();
+getTasksData();
 
 //#endregion
